b217a02a0891b1e8f2579e5e252eeb7b182188f1
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2023-05-14 15:05:51 +0200

    small fix

--- src/test/java/com/lukaszziobro/blogapp/PostServiceTest.java
+++ src/test/java/com/lukaszziobro/blogapp/PostServiceTest.java
@@ -1,5 +1,6 @@
 package com.lukaszziobro.blogapp;
 
+import com.lukaszziobro.blogapp.entity.Post;
 import com.lukaszziobro.blogapp.payload.PostDto;
 import com.lukaszziobro.blogapp.service.PostService;
 import org.assertj.core.api.Assertions;
@@ -12,6 +13,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 class PostServiceTest {
@@ -20,14 +24,28 @@ class PostServiceTest {
     private PostService postService;
 
     @Test
-    @DisplayName("Test init data loaded correctly")
-    public void testInitDataLoaded(){
+    @DisplayName("Test init data loaded correctly - count")
+    public void testInitDataLoadedCount(){
         Pageable page_0 = PageRequest.of(0,10);
         Pageable page_1 = PageRequest.of(1,10);
         Page<PostDto> postList0 = postService.getAllPosts(page_0);
         Page<PostDto> postList1 = postService.getAllPosts(page_1);
         Assertions.assertThat(postList0.getSize()).isEqualTo(10);
-        Assertions.assertThat(postList1.getSize()).isEqualTo(2);
+        Assertions.assertThat(postList1.getSize()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Test init data loaded correctly - sample")
+    public void testInitDataLoadedSample(){
+        Pageable page_0 = PageRequest.of(0,10);
+        List<PostDto> postList = postService.getAllPosts(page_0).stream().filter(
+                post -> post.getId().equals(3L)).toList();
+        Assertions.assertThat(postList.size()).isEqualTo(1);
+        PostDto post = postList.get(0);
+        Assertions.assertThat(post.getId()).isEqualTo(3L);
+        Assertions.assertThat(post.getTitle()).isEqualTo("title 3");
+        Assertions.assertThat(post.getContent()).isEqualTo("content 3");
+        Assertions.assertThat(post.getDescription()).isEqualTo("description 3");
     }
 
     @Test
