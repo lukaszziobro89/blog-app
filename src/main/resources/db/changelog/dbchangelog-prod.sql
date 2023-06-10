359dabd2083a6e07ebbd49b5fe69f3a79355f7aa
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2023-06-11 00:11:27 +0200

    roles

--- src/main/resources/db/changelog/dbchangelog-prod.sql
+++ src/main/resources/db/changelog/dbchangelog-prod.sql
@@ -17,3 +17,26 @@ create table myblog.comments
     post_id bigint       null,
     foreign key (post_id) references myblog.posts (id)
 );
+
+create table myblog.users
+(
+    id       bigint auto_increment primary key,
+    name     varchar(255),
+    username varchar(255) not null unique,
+    email    varchar(255) not null unique,
+    password varchar(255) not null
+);
+
+create table myblog.roles
+(
+    id   bigint auto_increment primary key,
+    name varchar(255)
+);
+
+create table myblog.users_roles
+(
+    user_id bigint not null,
+    role_id bigint not null,
+    FOREIGN KEY (user_id) REFERENCES myblog.users (id),
+    FOREIGN KEY (role_id) REFERENCES myblog.roles (id)
+);
